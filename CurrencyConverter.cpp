f2233162647945cdf344350ecd76b0636ae07a37
riyank456 <135857127+riyank456@users.noreply.github.com>
2023-09-12 22:32:50 -0400

    Add files via upload

---
+++ CurrencyConverter.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <algorithm>  // Include for std::transform
+#include <cctype>     // Include for std::tolower
+#include <vector> 
+
+
+using namespace std; 
+
+int main(){
+string yen = "Yen"; 
+string yuan = "Yuan"; 
+string cd = "Canadian Dollar"; 
+string pound = "Pound"; 
+double usd = 0;  
+cout<< "Welcome to the simple currency converter app \n Where you can convert USD to a currency of your choice \n"; 
+cout <<"Enter your USD amount below or enter 00 to exit!"<<endl; 
+cin >> usd; 
+cout <<"Great! Now what would you like to convert to? The options are listed below: \n-Yen\n-Pound\n-Canadian Dollar\n-Yuan\n"; 
+string convertTo; 
+cin >> convertTo; 
+
+if(convertTo == yen){
+    cout<<"The Yen equivalent of "<<usd<<" US dollars is "<<(usd*147.31)<<endl; 
+}
+
+else if(convertTo == yuan){
+     cout<<"The Yuan equivalent of "<<usd<<" US dollars is "<<(usd*7.29)<<endl;  
+}
+
+else if(convertTo == cd){
+     cout<<"The Canadian Dollar equivalent of "<<usd<<" US dollars is "<<(usd*1.36)<<endl;; 
+}
+
+else if(convertTo == pound){
+     cout<<"The Pound equivalent of "<<usd<<" US dollars is "<<(usd*.8)<<endl; 
+}
+
+else{
+    cout<<"Please enter one of the currencies listed above, make sure you spellcheck"; 
+}
+
+} 
